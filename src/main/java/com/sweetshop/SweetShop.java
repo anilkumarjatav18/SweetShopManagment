3d7fdb27544bd319afee9e0e99de719d917f1704
Anil kumar <anilkumarjatav18@gmail.com>
2025-07-17 11:14:26 +0530

    add sweet functionality

---
+++ src/main/java/com/sweetshop/SweetShop.java
@@ -0,0 +1,26 @@
+package com.sweetshop;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SweetShop {
+   // Map to store the Sweets
+    private Map<Integer, Sweet> sweets;
+
+    public SweetShop() {
+        this.sweets = new HashMap<>();
+    }
+
+    public void addSweet(Sweet sweet) {
+        if (sweets.containsKey(sweet.getId())) {
+            //throw error if sweet id already exist
+            throw new IllegalArgumentException("Sweet ID already exists");
+        }
+        sweets.put(sweet.getId(), sweet);
+    }
+    // method that return the  Number of sweets in the  store
+    public List<Sweet> viewSweets() {
+        return new ArrayList<>(sweets.values());
+    }
+
+}
